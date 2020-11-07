ec48384af3be69d111e12c5b0a36e1a2632c6662
Sebascam1498 <55771436+Sebascam1498@users.noreply.github.com>
2020-11-07 17:08:51 -0600

    Create ParserCordenadas.h

---
+++ ParserCordenadas.h
@@ -0,0 +1,55 @@
+#pragma once
+#include <string>
+#include <vector>
+
+//string prueba ="m 12,13 2,-3 z m 1,1 2,2 z";
+
+
+//Funcion que recibe como parametros el String de Cordenadas X y Y, devuelde la Sumatoria de ambas
+std::vector<float> ParserCordenadasXY(std::string pCordenadas) {
+
+    std::vector<float> CordenadasX{};
+    std::vector<float> CordenadasY{};
+
+    int largoString = pCordenadas.length();
+    float sumatoriaX = 0;
+    float sumatoriaY = 0;
+    std::string indiceX = "";
+    std::string indiceY = "";
+    int i = 2;
+    int indiceCordenadas = 0;
+
+    while (i < largoString) {
+        while ((pCordenadas.at(i) != *" ") && (pCordenadas.at(i) != *"z")) {
+
+            indiceX += pCordenadas.at(i);
+            i++;
+            if (pCordenadas.at(i) == *",") {
+                sumatoriaX += stof(indiceX);
+                CordenadasX.push_back(sumatoriaX);
+                i++;
+                while (pCordenadas.at(i) != *" ") {
+                    indiceY += pCordenadas.at(i);
+                    i++;
+                }
+                sumatoriaY += stof(indiceY);
+                CordenadasY.push_back(sumatoriaY);
+                indiceY = "";
+                indiceX = "";
+                i++;
+
+            }
+        }
+        if (pCordenadas.at(i) == *"z")
+        {
+            i += 4;
+            sumatoriaX = 0;
+            sumatoriaY = 0;
+            indiceY = " ";
+            indiceX = " ";
+        }
+    }
+    
+    return CordenadasX;
+
+}
